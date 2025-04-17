0cf268594713cac4d53c4730e12af311d923c28d
GuilhermexL <guilhermesantosfv@gmail.com>
2025-04-17 11:48:09 -0300

    feat: update de arquivos do frontend

--- diario-frontend/src/App.js
+++ diario-frontend/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Post from './Post';
+import NewPost from './NewPost';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="wrapper">
+        <header id="header">
+          <a href="/" className="logo">Meu Diário</a>
+        </header>
+
+        <nav id="nav">
+          <ul className="links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/new">Nova Postagem</a></li>
+          </ul>
+        </nav>
+
+        <div id="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="/new" element={<NewPost />} />
+          </Routes>
+        </div>
+
+        <footer id="footer">
+          <p>Feito com React e Massively</p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
